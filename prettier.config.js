b32dd8bbc0d5d122fb0fc07ea8687f4b8781714b
Carlos <carlosaac232001@gmail.com>
2025-08-19 23:38:43 -0500

    feat(prettier): add prettier config to project

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  printWidth: 80,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"], // <- opcional si usas Tailwind
+};
